76b32234368895ebb1dbda6917d001b23a165bb7
Zubair Khalid <zubairkhalidce@gmail.com>
2023-05-17 12:57:20 +0100

    added dev docs, fixed array out of bounds errors, changed some functions to be external only

--- contracts/MultiSigWallet.sol
+++ contracts/MultiSigWallet.sol
@@ -43,6 +43,17 @@ contract MultiSigWallet is AccessControl {
         _;
     }
 
+    /**
+     * @dev Constructor
+     * @param _token ERC20 token address
+     * @param _signers List of signer addresses (at least 1)
+     * @param _minConfirmationsToExecTx  Minimum number of confirmations required to execute tx
+     * @notice _minConfirmationsToExecTx must be less than or equal to the number of signers
+     * @notice _signers must be unique
+     * @notice _signers must not be the zero address
+     * @notice _token must be a valid ERC20 token
+     */
+
     constructor(address _token, address[] memory _signers, uint _minConfirmationsToExecTx) {
         
         require(_signers.length > 0, "Signers required");
@@ -72,6 +83,16 @@ contract MultiSigWallet is AccessControl {
 
     }
 
+    /**
+     * @dev Submit a transaction to be executed. It emits a SubmitTransaction event
+     * @param _to Address to send tokens to
+     * @param _value Amount of tokens to send
+     * @notice _to must not be the zero address
+     * @notice _value must be greater than 0
+     * @notice _value must be less than or equal to the contract's token balance
+     * @notice only signers can submit transactions
+     */
+
     function submitTransaction(
         address _to,
         uint256 _value
@@ -100,6 +121,15 @@ contract MultiSigWallet is AccessControl {
         emit SubmitTransaction(msg.sender, txIndex, _to, _value);
     }
 
+    /**
+     * @dev Confirm a transaction. It emits a ConfirmTransaction event
+     * @param _txIndex Transaction index to confirm
+     * @notice _txIndex must be less than the number of transactions
+     * @notice only signers can confirm transactions
+     * @notice only non-executed transactions can be confirmed
+     * @notice only non-confirmed transactions can be confirmed
+     */
+
     function confirmTransaction(
         uint256 _txIndex
     )
@@ -109,6 +139,9 @@ contract MultiSigWallet is AccessControl {
         notExecuted(_txIndex)
         notConfirmed(_txIndex)
     {
+
+        require(_txIndex < transactions.length, "tx does not exist");
+
         Transaction memory transaction = transactions[_txIndex];
 
         transaction.numConfirmations += 1;
@@ -119,9 +152,48 @@ contract MultiSigWallet is AccessControl {
         emit ConfirmTransaction(msg.sender, _txIndex);
     }
 
+    /**
+     * @dev Revoke a confirmation. It emits a RevokeConfirmation event
+     * @param _txIndex Transaction index to revoke confirmation
+     * @notice _txIndex must be less than the number of transactions
+     * @notice only signers can revoke confirmations
+     * @notice only non-executed transactions can be revoked
+     * @notice only confirmed transactions can be revoked
+     */
+
+    function revokeConfirmation(
+        uint256 _txIndex
+    ) external onlyRole(SIGNER_ROLE) txExists(_txIndex) notExecuted(_txIndex) {
+
+        require(_txIndex < transactions.length, "tx does not exist");
+
+        Transaction memory transaction = transactions[_txIndex];
+
+        require(isConfirmed[_txIndex][msg.sender], "Sorry, tx not confirmed");
+
+        unchecked {
+            transaction.numConfirmations = (transaction.numConfirmations - 1) == (2**256 - 1) ? 0 : (transaction.numConfirmations - 1);
+        }
+
+        isConfirmed[_txIndex][msg.sender] = false;
+
+        emit RevokeConfirmation(msg.sender, _txIndex);
+    }
+
+    /**
+     * @dev Execute a transaction. It emits a ExecuteTransaction event
+     * @param _txIndex Transaction index to execute
+     * @notice _txIndex must be less than the number of transactions
+     * @notice only signers can execute transactions
+     * @notice only non-executed transactions can be executed
+     * @notice only transactions with enough confirmations can be executed
+     */
+
     function executeTransaction(
         uint256 _txIndex
     ) external onlyRole(SIGNER_ROLE) txExists(_txIndex) notExecuted(_txIndex) {
+
+        require(_txIndex < transactions.length, "tx does not exist");
         
         Transaction memory transaction = transactions[_txIndex];
 
@@ -139,22 +211,12 @@ contract MultiSigWallet is AccessControl {
         emit ExecuteTransaction(msg.sender, _txIndex);
     }
 
-    function revokeConfirmation(
-        uint256 _txIndex
-    ) external onlyRole(SIGNER_ROLE) txExists(_txIndex) notExecuted(_txIndex) {
-
-        Transaction memory transaction = transactions[_txIndex];
-
-        require(isConfirmed[_txIndex][msg.sender], "Sorry, tx not confirmed");
-
-        unchecked {
-            transaction.numConfirmations = (transaction.numConfirmations - 1) == (2**256 - 1) ? 0 : (transaction.numConfirmations - 1);
-        }
-
-        isConfirmed[_txIndex][msg.sender] = false;
-
-        emit RevokeConfirmation(msg.sender, _txIndex);
-    }
+    /**
+     * @dev Grant signer role to an address. It emits a GrantSigner event
+     * @param _signer Address to grant signer role
+     * @notice only admin can grant signer role
+     * @notice _signer must not have signer role already
+     */
 
     function grantSigner(address _signer) external onlyRole(ADMIN_ROLE) {
         
@@ -166,6 +228,13 @@ contract MultiSigWallet is AccessControl {
         emit GrantSigner(_signer);
 
     }
+
+    /**
+     * @dev Revoke signer role from an address. It emits a RevokeSigner event
+     * @param _signer Address to revoke signer role
+     * @notice only admin can revoke signer role
+     * @notice _signer must have signer role already
+     */
 
     function revokeSigner(address _signer) external onlyRole(ADMIN_ROLE) {
         
@@ -184,21 +253,51 @@ contract MultiSigWallet is AccessControl {
         emit RevokeSigner(_signer);
     }
 
+    /**
+     * @dev Update the ERC20 token to be used. It emits a UpdateToken event.
+     * @param _token Address of the ERC20 token to be used
+     * @notice only admin can update the token
+     * @notice _token must not be the zero address
+     * @notice _token must be different from the current token
+     */
+
     function updateToken(address _token) external onlyRole(ADMIN_ROLE) {
+
+        require(_token != address(0), "token can't be zero address");
+        require(_token != address(token), "token already set");
+
         token = IERC20(_token);
+        emit UpdateToken(_token);
     }
 
-    function getSigners() public view returns (address[] memory) {
+    /**
+     * @dev Get the signers. It returns an array of addresses
+     */
+
+    function getSigners() external view returns (address[] memory) {
         return signers;
     }
 
-    function getTransactionCount() public view returns (uint256) {
+    /**
+     * @dev Get the number of transactions
+    */
+
+    function getTransactionCount() external view returns (uint256) {
         return transactions.length;
     }
 
+    /**
+     * @dev Get the transaction. It returns a Transaction struct
+     * @param _txIndex Transaction index to get
+     * @notice _txIndex must be less than the number of transactions
+     */
+
     function getTransaction(
         uint256 _txIndex
-    ) public view returns (Transaction memory transaction) {
+    ) external view returns (Transaction memory transaction) {
+
+        require(_txIndex < transactions.length, "tx does not exist");
+
         transaction = transactions[_txIndex];
     }
 
@@ -214,5 +313,6 @@ contract MultiSigWallet is AccessControl {
 
     event GrantSigner(address indexed signer);
     event RevokeSigner(address indexed signer);
+    event UpdateToken(address indexed token);
 
 }
